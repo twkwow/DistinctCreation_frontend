5650ff77049ae29a955a3bc613c58b31c20f8bca
twkwow <wenkanglol444@gmail.com>
2024-12-26 18:36:10 +0800

    updateupdate

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+1. Run "npm install"
+2. To run frontend, "npm run dev"
+3. Go to http://localhost:3000 on browser
+
+Tests done
+
+Test 1: Upload single image (> 128x128px) on "Story 1 & Story 3"
+Result: Single image uploaded to the "uploads" folder on backend, permenant & 2 thumbnail links generated ( 32px and 64px )
+
+Test 2: Upload single image (<> 128x128px) on "Story 1 & Story 3"
+Result: Single image uploaded to the "uploads" folder on backend, permenant & 1 thumbnail link generated ( its own size <128px )
+
+Test 3: Upload single image on "Story 2.1"
+Result: Single image uploaded to the "uploads" folder on backend, permenant link generated.
+
+Test 4: Upload a ZIP File containing images on "Story 2.1"
+Result: Extracts the ZIP folder, then the images uploaded to the "uploads" folder on backend, multiple permenant link generated.
--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,196 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+"use client";
+
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { FilePond, File, registerPlugin } from "react-filepond";
+import "filepond/dist/filepond.min.css";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css"; // Import Image Preview styles
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import FilePondPluginFileEncode from "filepond-plugin-file-encode";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import { Button } from "@mui/material";
+
+registerPlugin(
+  FilePondPluginImagePreview,
+  FilePondPluginFileEncode,
+  FilePondPluginFileValidateType
+);
 
 export default function Home() {
+  const [files, setFiles] = useState([]);
+  const [files2, setFiles2] = useState([]);
+  const [link, setLink] = useState("");
+  const [link2, setLink2] = useState([]);
+  const [thumbnails, setThumbnails] = useState([]);
+
+  const handleUpload = async () => {
+    console.log(files, "files");
+    if (files.length === 0) {
+      alert("Please select an image to upload.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("picture", files[0]);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/uploadSingle",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          validateStatus: function (status) {
+            return status < 500;
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        console.log("Upload success");
+        setLink(response.data.fileUrl);
+        setThumbnails(response.data.thumbnails);
+        alert("Upload success");
+      } else if (response.status === 400) {
+        console.log("Invalid file or something");
+        alert("Invalid file?");
+      }
+    } catch (error) {
+      console.log("error here", error);
+    }
+  };
+
+  const handleUploadZip = async () => {
+    console.log(files2, "files2");
+    if (files2.length === 0) {
+      alert("Please select a ZIP file to upload.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", files2[0]);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/uploadSingleOrZip",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          validateStatus: function (status) {
+            return status < 500;
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        console.log("Upload success");
+        setLink2([response.data.fileUrl]);
+        alert("Upload success");
+      } else if (response.status === 201) {
+        console.log("Upload success");
+        setLink2(response.data.imageLinks);
+        console.log(response.data.imageLinks);
+        alert("Upload success");
+      } else if (response.status === 400) {
+        console.log("Invalid file or something");
+        alert("Invalid file?");
+      }
+    } catch (error) {
+      console.log("error here", error);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
+        {/* story 1 & 3 */}
+        <h1 className={styles.title}>Story 1 & Story 3</h1>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a className={styles.card}>
+            <h3>Upload Image Only</h3>
+            {/* only accept picture, otherwise not allow */}
+            <FilePond
+              files={files}
+              onupdatefiles={(fileItems) => {
+                setFiles(fileItems.map((fileItem) => fileItem.file));
+              }}
+              acceptedFileTypes={["image/*"]}
+              allowMultiple={false}
+              name="picture"
+              labelIdle='Drag & Drop your image or <span class="filepond--label-action">Browse</span>'
+            />
+            <Button onClick={handleUpload}>Upload</Button>
+            {link && (
+              <div>
+                <p>
+                  Permanent Link:{" "}
+                  <a href={link} target="_blank" rel="noopener noreferrer">
+                    {link}
+                  </a>
+                </p>
+                {thumbnails.length > 0 && (
+                  <div>
+                    <h4>Thumbnails:</h4>
+                    {thumbnails.map((thumbnail, index) => (
+                      <p key={index}>
+                        <a
+                          href={thumbnail}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {thumbnail}
+                        </a>
+                      </p>
+                    ))}
+                  </div>
+                )}
+              </div>
+            )}
           </a>
+        </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+        {/* story 2 */}
+        <h1 className={styles.title}>Story 2.1</h1>
+        <div className={styles.grid}>
+          <a className={styles.card}>
+            <h3>Upload Zip and Extract Image </h3>
+            {/* only accept picture, otherwise not allow */}
+            <FilePond
+              files={files2}
+              onupdatefiles={(fileItems) => {
+                setFiles2(fileItems.map((fileItem) => fileItem.file));
+              }}
+              acceptedFileTypes={["image/*", "application/x-zip-compressed"]}
+              allowMultiple={false}
+              name="picture"
+              labelIdle='Drag & Drop your image or <span class="filepond--label-action">Browse</span>'
+            />
+            <Button onClick={handleUploadZip}>Upload</Button>
+            {link2.length > 0 && (
+              <div>
+                <h4>Permanent Links:</h4>
+                {link2.map((link, index) => (
+                  <p key={index}>
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                      {link}
+                    </a>
+                  </p>
+                ))}
+              </div>
+            )}
           </a>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
